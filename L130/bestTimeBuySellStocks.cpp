3f23c90a4dfaaac408d962fff4d68634cb442bbc
AllenSabu <94732272+InfinityDemon2001@users.noreply.github.com>
2024-07-28 11:39:03 +0530

    .

---
+++ L130/bestTimeBuySellStocks.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+// Method 1: Simple for loop
+int maxProfit(vector<int>& prices) {
+    int maxi = 0, mini = INT_MAX;
+    
+    for(int i = 0; i < prices.size(); i ++) {
+        mini = min(mini, prices[i]);
+        maxi = max(maxi, prices[i] - mini);
+    }
+
+    return maxi;
+}
+
